3e89d844bcfe611d310f28cc5b275963a430335f
Danil01042005 <skotdaivanov@gmail.com>
2025-08-06 01:31:48 +0500

    Add UserRepository enabling CRUD operations for User entities

---
+++ src/main/java/ru/lms/gatewayauth/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package ru.lms.gatewayauth.repository;
+
+import ru.lms.gatewayauth.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
